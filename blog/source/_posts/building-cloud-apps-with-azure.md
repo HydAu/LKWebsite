deb60cd7bd3a48e83de1c870bfb16aea4195dadd
Laure Kamalandua <laure.kamalandua@gmail.com>
2016-04-28 19:21:42 +0200

    blog post

---
+++ blog/source/_posts/building-cloud-apps-with-azure.md
@@ -0,0 +1,26 @@
+title: Building Cloud apps with Microsoft Azure (Freebie)
+date: 2016-04-28 18:38:55
+tags:
+- Microsoft Developer
+- Azure
+- Development
+coverImage: https://cdn.pbrd.co/images/wNAdLQU.png
+---
+
+Roaming the web and following the Microsoft developer page through every channel really has its benefits. Today I discovered an incredibly valuable book to get started with building apps on the Azure stack. 198 pages on the topic of tailoring applications for cloud environments<!-- more -->. Definitely tune in if you're interested about:
+<br>
+
+- Automation
+- Source control
+- Continuous integration and delivery 
+- Single sign on
+- Data storage options
+- Monitoring
+- Web development best practices 
+- Distributed caching  
+- Surviving failures 
+<br>
+{% image fancybox center https://cdn.pbrd.co/images/wNQXYPj.png %}
+
+<center>Download the {% hl_text purple %}Building cloud apps with Microsoft Azure{% endhl_text %} book from [HERE](https://onedrive.live.com/redir?resid=237E9F72C4D765FF!2281&authkey=!AA1E8sI9DawFXIc&ithint=file%2cpdf)</center>
+
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,39 @@
+// jQuery fix blog content previews 
+/*function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        // XHR has 'withCredentials' property only if it supports CORS
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") { // if IE use XDR
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        xhr = null;
+    }
+    return xhr;
+}
+
+var request = createCORSRequest("get", "http://localhost:3000/");
+if (request) {
+    // Define a callback funct,ion
+    request.onload = function() {
+        //console.log(request.response);
+        var title_one = document.getElementById('bg-title-one');
+        var title_two = document.getElementById('bg-title-two');
+        var title_three = document.getElementById('bg-title-three');
+        var title_one_n = one.innerHTML;
+        var title_two_n = two.innerHTML;
+        var title_three_n = three.innerHTML;
+        title_one.innerHTML = title_one_n;
+        title_two.innerHTML = title_two_n;
+        title_three.innerHTML = title_three_n;
+    };
+    // Send request
+    request.send();
+}*/
+
 // jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
@@ -8,8 +42,8 @@ $(window).scroll(function() {
 });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
+$(function () {
+    $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
@@ -19,8 +53,7 @@ $(function() {
 });
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+$('.navbar-collapse ul li a').click(function () {
     $('.navbar-toggle:visible').click();
 });
 
-http.request(options, callback).end();
